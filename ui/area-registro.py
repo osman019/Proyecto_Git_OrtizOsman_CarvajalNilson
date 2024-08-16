d70d061a15deaee879b8393866757255e9b0588a
nilsoncarvajal67950 <nilsoncarvajal567@gmail.com>
2024-08-16 15:39:43 -0500

    interfaz de datos

---
+++ ui/area-registro.py
@@ -0,0 +1,10 @@
+def registro(op):
+ print(title)
+ 
+    
+ title =("""
+
+----------INGRESA TUS DATOS----------- """)
+ 
+ print(mainregistroop)
+ mainregistroop = "1. Nombre de su sucursal :\n2. Dirección de su sucursal :\n3. Número de teléfono o correo :\n4. ID GERENTE:  "
